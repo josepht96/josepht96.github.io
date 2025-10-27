707a9dc348e52e132408f223ca9a500c2a2fb5af
josepht96 <joebthomas4@gmail.com>
2025-10-27 14:40:43 -0500

    add

--- app/routes.ts
+++ app/routes.ts
@@ -2,5 +2,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("*", "routes/catch-all.tsx"),
+    // route("*", "routes/catch-all.tsx"),
 ] satisfies RouteConfig;
